8bb8d441a08d6d8d795fa63f624a7e388125a618
evertonjovito <evertonjovitooi@gmail.com>
2022-12-19 15:02:43 -0300

    Commit

---
+++ Master Vault/MasterVault1.sol
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.7;
+
+contract EFTStaking {
+    // Tipo de dados para representar uma entrada de stake de um usuário
+    struct Stake {
+        address owner; // endereço do proprietário do stake
+        uint256 amount; // quantidade de tokens EFT em stake
+        uint256 lastClaimTime; // último momento em que o usuário reclamou seus tokens TBNB
+    }
+
+    // Mapeia cada endereço para sua entrada de stake
+    mapping(address => Stake) public stakes;
+
+    // Quantidade total de tokens EFT em stake
+    uint256 public totalStakedAmount;
+
+    // Quantidade total de tokens TBNB disponíveis
+    uint256 public totalTBNBAmount;
+
+    // Evento disparado quando um novo stake é adicionado
+    event Staked(address indexed owner, uint256 amount);
+
+    // Evento disparado quando um stake é removido
+    event Unstaked(address indexed owner, uint256 amount);
+
+    // Evento disparado quando tokens TBNB são distribuídos
+    event TBNBDistributed(address indexed owner, uint256 amount);
+
+    // Adiciona um novo stake de tokens EFT
+    function stake(uint256 amount) public payable {
+        require(msg.value == amount, "O valor enviado deve ser igual aa quantidade de tokens EFT em stake");
+
+        // Atualiza a entrada de stake do usuário
+        Stake storage stake = stakes[msg.sender];
+        stake.amount += amount;
+        stake.lastClaimTime = now;
+
+        // Atualiza a quantidade total de tokens EFT em stake
+        totalStakedAmount += amount;
+
+        // Dispara o evento Staked
+        emit Staked(msg.sender, amount);
+    }
+
+    // Remove um stake de tokens EFT
+    function unstake(uint256 amount) public {
+        Stake storage stake = stakes[msg.sender];
+        require(stake.amount >= amount, "Voce nao tem tokens EFT suficientes em stake");
+
+        // Atualiza a entrada de stake do usuário
+        stake.amount -= amount;
+
+        // Atualiza a quantidade total de tokens EFT em stake
+        totalStakedAmount -= amount;
+
+        // Dispara o evento Unstaked
+        emit Unstaked(msg.sender, amount);
+
+        // Envia os tokens EFT de volta para o usuário
+        msg.sender.transfer(amount);
+    }
+
+    // Reclama os tokens TBNB que o usuário tem direito com base em seu stake de tokens EFT
+function claim() public {
+    Stake storage stake = stakes[msg.sender];
+    require(stake.amount > 0, "Voce nao tem tokens EFT em stake");
+
+    // Calcula a quantidade de tokens TBNB a ser distribuída para o usuário com base em sua participação no staking
+    uint256 tokensToClaim = stake.amount * totalTBNBAmount / totalStakedAmount;
+
+    // Atualiza a última vez que o usuário reclamou tokens TBNB
+    stake.lastClaimTime = now;
+
+    // Atualiza a quantidade total de tokens TBNB disponíveis
+    totalTBNBAmount -= tokensToClaim;
+
+    // Dispara o evento TBNBDistributed
+    emit TBNBDistributed(msg.sender, tokensToClaim);
+
+    // Envia os tokens TBNB para o usuário
+    msg.sender.transfer(tokensToClaim);
+}
