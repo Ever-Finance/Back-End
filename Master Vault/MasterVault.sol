8dfbca9bf30639aeef8f8ae57c8cead79fe8af00
evertonjovito <evertonjovitooi@gmail.com>
2022-12-20 16:00:59 -0300

    ok

--- Master Vault/MasterVault.sol
+++ Master Vault/MasterVault.sol
@@ -1,13 +1,11 @@
+//Pragma
 pragma solidity ^0.8.7;
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7f6a1666fac8ecff5dd467d0938069bc221ea9e0/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7f6a1666fac8ecff5dd467d0938069bc221ea9e0/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // Identificador de licença
 //SPDX-License-Identifier: MIT
-
-// Cláusula de pragma
-pragma solidity ^0.8.7;
 
 // Contrato EFTStaking
 contract EFTStaking {
@@ -60,9 +58,13 @@ contract EFTStaking {
 
     // Essa função tem o objetivo de distribuir as recompensas para os stakers
     function distributeStakeRewards(uint256 value) public {
+        // Verifique se o valor é positivo
+        require(value > 0, "O valor deve ser positivo");
+
         // Obtenha a lista de endereços que têm tokens em stake
         address[] stakers = getStakers();
-                // Obtenha o número de tokens em stake de cada staker
+
+        // Obtenha o número de tokens em stake de cada staker
         uint256[] stakeAmounts = new uint256[](stakers.length);
         for (uint i = 0; i < stakers.length; i++) {
             stakeAmounts[i] = getStakeAmount(stakers[i]);
@@ -83,60 +85,68 @@ contract EFTStaking {
     // Essa função tem o objetivo de resgatar as recompensas
     // sem retirar os tokens em stake
     function withdrawRewards() public {
-        // Obtenha o saldo de recompensas do staker
-        uint256 rewardBalance = ERC20(platformTokenAddress).balanceOf(address(this));
+        // Verifique se o staker tem saldo de recompensas
+        require(stakes[msg.sender] > 0, "Saldo de recompensas insuficiente");
 
-        // Verifique se o staker tem saldo de recompensas para resgatar
-        require(rewardBalance > 0, "Saldo de recompensas insuficiente");
-
-        // Transfira as recompensas para o staker
-        ERC20(platformTokenAddress).transfer(msg.sender, rewardBalance);
+        // Retire as recompensas do staker
+        uint256 rewards = stakes[msg.sender];
+        stakes[msg.sender] = 0;
 
         // Emita um evento para registrar o resgate de recompensas
-        emit RewardsWithdrawn(msg.sender, rewardBalance);
+        emit RewardsWithdrawn(msg.sender, rewards);
     }
 
-    // Essa função tem o objetivo de redistribuir as recompensas
-    // se elas não forem resgatadas após 1 década
-    function redistributeRewards() public {
-        // Verifique se o contrato tem saldo de recompensas para redistribuir
-        uint256 rewardBalance = ERC20(platformTokenAddress).balanceOf(address(this));
-        require(rewardBalance > 0, "Nao ha recompensas para redistribuir");
+    // Essa função tem o objetivo de redistribuir recompensas não resgatadas
+    // após um período de 1 década
+    function redistributeExpiredRewards() public {
+        // Obtenha a lista de stakers
+        address[] stakers = getStakers();
 
-        // Distribua as recompensas para os stakers
-        distributeStakeRewards(rewardBalance);
-    }
-
-    // Essa função tem o objetivo de obter a lista de endereços que têm tokens em stake
-    function getStakers() public view returns (address[] memory) {
-        // Inicialize a lista de stakers
-        address[] memory stakers = new address[](stakes.length);
-
-        // Preencha a lista de stakers
-        uint i = 0;
-        for (address staker = stakes.keys(); staker != address(0); staker = stakes.nextKey(staker)) {
-            stakers[i] = staker;
-            i++;
+        // Obtenha o saldo de recompensas não resgatadas de cada staker
+        uint256[] rewardAmounts = new uint256[](stakers.length);
+        for (uint i = 0; i < stakers.length; i++) {
+            rewardAmounts[i] = stakes[stakers[i]];
         }
-            // Retorne a lista de stakers
+
+        // Calcule o total de recompensas não resgatadas
+        uint256 totalExpiredRewards = 0;
+        for (uint i = 0; i < stakers.length; i++) {
+            totalExpiredRewards = totalExpiredRewards.add(rewardAmounts[i]);
+        }
+
+        // Verifique se existem recompensas não resgatadas
+        require(totalExpiredRewards > 0, "Nao ha recompensas nao resgatadas para redistribuir");
+
+        // Redistribua as recompensas não resgatadas para os stakers
+        for (uint i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            uint256 rewardAmount = rewardAmounts[i];
+            uint256 reward = rewardAmount * totalStake / totalExpiredRewards;
+            staker.transfer(reward);
+        }
+
+        // Emita um evento para registrar a redistribuição de recompensas
+        emit RewardsRedistributed(totalExpiredRewards);
+    }
+
+    // Essa função tem o objetivo de obter a lista de stakers
+    function getStakers() public view returns (address[] memory) {
+        // Inicialize uma lista vazia
+        address[] memory stakers = new address[](0);
+
+        // Percorra o mapeamento de stakes e adicione os stakers à lista
+        for (address staker in stakes) {
+            if (stakes[staker] > 0) {
+                stakers.push(staker);
+            }
+        }
+
+        // Retorne a lista de stakers
         return stakers;
     }
 
     // Essa função tem o objetivo de obter o número de tokens em stake de um staker
     function getStakeAmount(address staker) public view returns (uint256) {
-        // Retorne o saldo de tokens em stake do staker
         return stakes[staker];
     }
-
-    // Evento para registrar a adição de tokens em stake
-    event Staked(address staker, uint256 amount);
-
-    // Evento para registrar a remoção de tokens em stake
-    event Unstaked(address staker, uint256 amount);
-
-    // Evento para registrar a distribuição de recompensas
-    event RewardsDistributed(uint256 value);
-
-    // Evento para registrar o resgate de recompensas
-    event RewardsWithdrawn(address staker, uint256 amount);
 }
