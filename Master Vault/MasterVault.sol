adff4cfe221768d4f77a3b3645670be8cdcc7268
evertonjovito <evertonjovitooi@gmail.com>
2022-12-20 17:09:40 -0300

    ok

--- Master Vault/MasterVault.sol
+++ Master Vault/MasterVault.sol
@@ -1,13 +1,10 @@
 //Pragma
 pragma solidity ^0.8.7;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7f6a1666fac8ecff5dd467d0938069bc221ea9e0/contracts/utils/math/SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7f6a1666fac8ecff5dd467d0938069bc221ea9e0/contracts/token/ERC20/utils/SafeERC20.sol";
-
 // Identificador de licença
 //SPDX-License-Identifier: MIT
 
 // Contrato EFTStaking
 contract EFTStaking {
-    // Importe a biblioteca SafeMath
+    
 }
